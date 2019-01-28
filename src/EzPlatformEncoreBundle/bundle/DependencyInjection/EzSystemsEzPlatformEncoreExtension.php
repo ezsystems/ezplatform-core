4cdb654fd5d6e12c3d80ab7dddc09cb1536215cf
Nattfarinn <nattfarinn@gmail.com>
2019-01-28 09:02:39 +0100

    EZP-30035: Install Webpack Encore

---
+++ src/EzPlatformEncoreBundle/bundle/DependencyInjection/EzSystemsEzPlatformEncoreExtension.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformEncoreBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class EzSystemsEzPlatformEncoreExtension extends Extension
+{
+    const EZ_ENCORE_CONFIG_NAME = 'ez.config.js';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $this->dumpConfigurationPathsToFile(
+            dirname($container->getParameter('kernel.root_dir')) . '/var/encore',
+            $container->getParameter('kernel.bundles_metadata')
+        );
+    }
+
+    /**
+     * Looks for Resources/encore/ez.config.js file in every registered and enabled bundle.
+     * Dumps json list of paths to files it finds.
+     *
+     * @param string $targetPath Where to put eZ Encore paths configuration file (default: var/encore/ez.config.js)
+     * @param array $bundlesMetadata
+     */
+    public function dumpConfigurationPathsToFile(string $targetPath, array $bundlesMetadata): void
+    {
+        $finder = new Finder();
+        $filesystem = new Filesystem();
+        $paths = [];
+
+        $finder
+            ->in(array_column($bundlesMetadata, 'path'))
+            ->path('Resources/encore')
+            ->name(self::EZ_ENCORE_CONFIG_NAME)
+            ->files();
+
+        foreach ($finder as $fileInfo) {
+            $paths[] = $fileInfo->getRealPath();
+        }
+
+        $filesystem->mkdir($targetPath);
+        $filesystem->dumpFile(
+            $targetPath . '/' . self::EZ_ENCORE_CONFIG_NAME,
+            sprintf('module.exports = %s', json_encode($paths))
+        );
+    }
+}
---
+++ src/EzPlatformEncoreBundle/bundle/EzSystemsEzPlatformEncoreBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformEncoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsEzPlatformEncoreBundle extends Bundle
+{
+}
