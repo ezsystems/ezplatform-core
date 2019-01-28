2116cbfe112d8dc68f2633e8b5231a70e75529e2
Nattfarinn <nattfarinn@gmail.com>
2019-01-28 09:02:39 +0100

    EZP-30035: Install Webpack Encore

---
+++ src/EncoreBundle/bundle/EzSystemsEncoreBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * File containing the EzSystemsEzSupportToolsBundle class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EncoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsEncoreBundle extends Bundle
+{
+}
