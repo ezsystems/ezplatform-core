b639fccdecb8cd70294e4426f1fbdb8235840e95
Nattfarinn <nattfarinn@gmail.com>
2019-03-25 10:28:12 +0100

    EZP-30345: Fixed missing webpack encore JS/CSS overriding from bundle

--- src/EzPlatformEncoreBundle/bundle/DependencyInjection/EzSystemsEzPlatformEncoreExtension.php
+++ src/EzPlatformEncoreBundle/bundle/DependencyInjection/EzSystemsEzPlatformEncoreExtension.php
@@ -16,26 +16,30 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 class EzSystemsEzPlatformEncoreExtension extends Extension
 {
     const EZ_ENCORE_CONFIG_NAME = 'ez.config.js';
+    const EZ_ENCORE_MANAGER_NAME = 'ez.config.manager.js';
 
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $this->dumpConfigurationPathsToFile(
-            dirname($container->getParameter('kernel.root_dir')) . '/var/encore',
-            $container->getParameter('kernel.bundles_metadata')
-        );
+        $bundlesMetadata = $container->getParameter('kernel.bundles_metadata');
+        $rootPath = dirname($container->getParameter('kernel.root_dir'));
+        $targetPath = 'var/encore';
+
+        $this->dumpConfigurationPathsToFile($rootPath, $targetPath, $bundlesMetadata);
+        $this->dumpConfigurationManagerPathsToFile($rootPath, $targetPath, $bundlesMetadata);
     }
 
     /**
      * Looks for Resources/encore/ez.config.js file in every registered and enabled bundle.
      * Dumps json list of paths to files it finds.
      *
+     * @param string $rootPath
      * @param string $targetPath Where to put eZ Encore paths configuration file (default: var/encore/ez.config.js)
      * @param array $bundlesMetadata
      */
-    public function dumpConfigurationPathsToFile(string $targetPath, array $bundlesMetadata): void
+    public function dumpConfigurationPathsToFile(string $rootPath, string $targetPath, array $bundlesMetadata): void
     {
         $finder = new Finder();
         $filesystem = new Filesystem();
@@ -47,14 +51,55 @@ class EzSystemsEzPlatformEncoreExtension extends Extension
             ->name(self::EZ_ENCORE_CONFIG_NAME)
             ->files();
 
+        /** @var \Symfony\Component\Finder\SplFileInfo $fileInfo */
         foreach ($finder as $fileInfo) {
-            $paths[] = $fileInfo->getRealPath();
+            $paths[] = preg_replace(
+                '/^' . preg_quote($rootPath, '/') . '/',
+                '.',
+                $fileInfo->getRealPath()
+            );
         }
 
         $filesystem->mkdir($targetPath);
         $filesystem->dumpFile(
-            $targetPath . '/' . self::EZ_ENCORE_CONFIG_NAME,
-            sprintf('module.exports = %s', json_encode($paths))
+            $rootPath . '/' . $targetPath . '/' . self::EZ_ENCORE_CONFIG_NAME,
+            sprintf('module.exports = %s;', json_encode($paths))
+        );
+    }
+
+    /**
+     * Looks for Resources/encore/ez.config.manager.js file in every registered and enabled bundle.
+     * Dumps json list of paths to files it finds.
+     *
+     * @param string $rootPath
+     * @param string $targetPath Where to put eZ Encore paths manager file (default: var/encore/ez.config.manager.js)
+     * @param array $bundlesMetadata
+     */
+    public function dumpConfigurationManagerPathsToFile(string $rootPath, string $targetPath, array $bundlesMetadata): void
+    {
+        $finder = new Finder();
+        $filesystem = new Filesystem();
+        $paths = [];
+
+        $finder
+            ->in(array_column($bundlesMetadata, 'path'))
+            ->path('Resources/encore')
+            ->name(self::EZ_ENCORE_MANAGER_NAME)
+            ->files();
+
+        /** @var \Symfony\Component\Finder\SplFileInfo $fileInfo */
+        foreach ($finder as $fileInfo) {
+            $paths[] = preg_replace(
+                '/^' . preg_quote($rootPath, '/') . '/',
+                '.',
+                $fileInfo->getRealPath()
+            );
+        }
+
+        $filesystem->mkdir($targetPath);
+        $filesystem->dumpFile(
+            $rootPath . '/' . $targetPath . '/' . self::EZ_ENCORE_MANAGER_NAME,
+            sprintf('module.exports = %s;', json_encode($paths))
         );
     }
 }
