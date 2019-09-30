02ee7aec1507673813d2fceca57efb07db1e037f
Andrew Longosz <alongosz@users.noreply.github.com>
2019-09-30 17:51:29 +0200

    EZP-30636: Provided forward compatibility for eZ Platform Extension (#11)
    * [Tests] Created initial PHPUnit tests setup
    
    * [Travis] Added PHPUnit unit tests job
    
    * [Composer] Added EzPlatformCoreBundle namespace to autoloader
    
    * [Tests] Created EzPlatformCoreBundle test suite
    
    * [Symfony] Created eZ Platform Core Bundle, Extension, and Configuration
    
    * [Symfony] Prepended ezplatform configuration to ezpublish extension
    
      This is a workaround to avoid copying all the code from EzPublishCoreBundle at once. It allows using "ezplatform" as a root node for the configuration w/o defining that configuration.
      Validation is still done by ezpublish extension.

---
+++ src/EzPlatformCoreBundle/bundle/DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformCoreBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+
+        $rootNode = $treeBuilder->root(EzPlatformCoreExtension::EXTENSION_ALIAS);
+
+        // @todo define actual configuration (move from EzPublishCoreBundle)
+        $rootNode->variablePrototype();
+
+        return $treeBuilder;
+    }
+}
---
+++ tests/EzPlatformCoreBundle/bundle/EzPlatformCoreBundleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\Tests\EzPlatformCoreBundle;
+
+use EzSystems\EzPlatformCoreBundle\EzPlatformCoreBundle;
+use PHPUnit\Framework\TestCase;
+
+final class EzPlatformCoreBundleTest extends TestCase
+{
+    public function testInstantiateBundle(): void
+    {
+        $bundle = new EzPlatformCoreBundle();
+        self::assertEquals('EzPlatformCoreBundle', $bundle->getName());
+        self::assertEquals('ezplatform', $bundle->getContainerExtension()->getAlias());
+    }
+}
