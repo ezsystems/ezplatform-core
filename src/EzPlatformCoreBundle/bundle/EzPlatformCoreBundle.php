c2bc4ddfe751ed3e0c1390f4e855a7bf9eaf0431
Łukasz Serwatka <lukasz.serwatka@ez.no>
2020-06-12 20:35:29 +0200

    Bump version to 3.0.5 (#18)

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -17,7 +17,7 @@ final class EzPlatformCoreBundle extends Bundle
     /**
      * eZ Platform Version.
      */
-    public const VERSION = '3.0.4';
+    public const VERSION = '3.0.5';
 
     public function getContainerExtension(): ExtensionInterface
     {
