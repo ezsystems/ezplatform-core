56b0f19b6450271891ba2863de09c8d177040bab
Andrew Longosz <alongosz@users.noreply.github.com>
2020-09-07 12:46:52 +0200

    Bumped eZ Platform version to 3.1.3

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -17,7 +17,7 @@ final class EzPlatformCoreBundle extends Bundle
     /**
      * eZ Platform Version.
      */
-    public const VERSION = '3.1.2';
+    public const VERSION = '3.1.3';
 
     public function getContainerExtension(): ExtensionInterface
     {
