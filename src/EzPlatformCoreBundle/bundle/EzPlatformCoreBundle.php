5394083aa453521aaefb3ed2ab1e8af27e868362
Maciej Kobus <webhdx@users.noreply.github.com>
2020-12-09 10:47:08 +0100

    EZP-32214: Refactored config/packages/overrides/* as Compiler Passes (#25)

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -8,7 +8,11 @@ declare(strict_types=1);
 
 namespace EzSystems\EzPlatformCoreBundle;
 
+use EzSystems\EzPlatformCoreBundle\DependencyInjection\Compiler\GenericConfigPass;
+use EzSystems\EzPlatformCoreBundle\DependencyInjection\Compiler\PlatformShConfigPass;
 use EzSystems\EzPlatformCoreBundle\DependencyInjection\EzPlatformCoreExtension;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -22,5 +26,13 @@ final class EzPlatformCoreBundle extends Bundle
     public function getContainerExtension(): ExtensionInterface
     {
         return new EzPlatformCoreExtension();
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new GenericConfigPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 255);
+        $container->addCompilerPass(new PlatformShConfigPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 250);
     }
 }
