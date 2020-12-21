3afd297d67ba77ffea9a8a79e7f455308b59a0b6
Maciej Kobus <maciej.kobus@ez.no>
2020-12-21 09:47:25 +0100

    EZP-32214: Moved parameters setup to compiler passes

---
+++ src/EzPlatformCoreBundle/bundle/DependencyInjection/Compiler/GenericConfigPass.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+namespace EzSystems\EzPlatformCoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class GenericConfigPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $projectDir = $container->getParameter('kernel.project_dir');
+
+        if ($dfsNfsPath = $_SERVER['DFS_NFS_PATH'] ?? false) {
+            $container->setParameter('dfs_nfs_path', $dfsNfsPath);
+
+            $parameterMap = [
+                'dfs_database_charset' => 'database_charset',
+                'dfs_database_driver' => 'database_driver',
+                'dfs_database_collation' => 'database_collation',
+            ];
+
+            foreach ($parameterMap as $dfsParameter => $platformParameter) {
+                $container->setParameter(
+                    $dfsParameter,
+                    $_SERVER[strtoupper($dfsParameter)] ?? $container->getParameter($platformParameter)
+                );
+            }
+        }
+
+        if ($purgeType = $_SERVER['HTTPCACHE_PURGE_TYPE'] ?? false) {
+            $container->setParameter('purge_type', $purgeType);
+            $container->setParameter('ezpublish.http_cache.purge_type', $purgeType);
+        }
+
+        if ($value = $_SERVER['MAILER_TRANSPORT'] ?? false) {
+            $container->setParameter('mailer_transport', $value);
+        }
+
+        if ($value = $_SERVER['LOG_TYPE'] ?? false) {
+            $container->setParameter('log_type', $value);
+        }
+
+        if ($value = $_SERVER['SESSION_HANDLER_ID'] ?? false) {
+            $container->setParameter('ezplatform.session.handler_id', $value);
+        }
+
+        if ($value = $_SERVER['SESSION_SAVE_PATH'] ?? false) {
+            $container->setParameter('ezplatform.session.save_path', $value);
+        }
+    }
+}
--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -8,7 +8,11 @@ declare(strict_types=1);
 
 namespace EzSystems\EzPlatformCoreBundle;
 
+use EzSystems\EzPlatformCoreBundle\DependencyInjection\Compiler\GenericConfigPass;
+use EzSystems\EzPlatformCoreBundle\DependencyInjection\Compiler\PlatformShConfigPass;
 use EzSystems\EzPlatformCoreBundle\DependencyInjection\EzPlatformCoreExtension;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -22,5 +26,11 @@ final class EzPlatformCoreBundle extends Bundle
     public function getContainerExtension(): ExtensionInterface
     {
         return new EzPlatformCoreExtension();
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new GenericConfigPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1000);
+        $container->addCompilerPass(new PlatformShConfigPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1000);
     }
 }
