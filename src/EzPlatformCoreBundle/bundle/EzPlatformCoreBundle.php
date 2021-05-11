2777aa44c7ff6907e7e103e7bab0d0688fe518e7
Andrew Longosz <alongosz@users.noreply.github.com>
2021-05-11 11:27:07 +0200

    Bumped Ibexa DXP FTR version to v3.2.7

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -17,7 +17,7 @@ final class EzPlatformCoreBundle extends Bundle
     /**
      * Ibexa DXP Version.
      */
-    public const VERSION = '3.2.6';
+    public const VERSION = '3.2.7';
 
     public function getContainerExtension(): ExtensionInterface
     {
