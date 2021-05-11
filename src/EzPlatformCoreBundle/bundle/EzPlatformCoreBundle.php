f4d62febfbaec4ea4cdbe05eab470a04f8939dde
Andrew Longosz <alongosz@users.noreply.github.com>
2021-05-11 11:47:00 +0200

    Bumped Ibexa version to v4.0.0

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -17,7 +17,7 @@ final class EzPlatformCoreBundle extends Bundle
     /**
      * Ibexa DXP Version.
      */
-    public const VERSION = '3.3.3';
+    public const VERSION = '4.0.0';
 
     public function getContainerExtension(): ExtensionInterface
     {
