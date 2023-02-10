bf43e7a53869c6b411dda1b6b07ccec3e56a5736
Andrew Longosz <alongosz@users.noreply.github.com>
2023-02-10 11:01:36 +0100

    Bumped Ibexa LTS version to v3.3.31

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -19,7 +19,7 @@ final class EzPlatformCoreBundle extends Bundle
     /**
      * Ibexa DXP Version.
      */
-    public const VERSION = '3.3.30';
+    public const VERSION = '3.3.31';
 
     public function getContainerExtension(): ExtensionInterface
     {
