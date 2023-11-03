c258a14603fc0ea4ca557b001b6f7f93d43951eb
Maciej Kobus <webhdx@users.noreply.github.com>
2023-11-03 13:31:33 +0100

    Bumped Ibexa LTS version to v3.3.37

--- src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
+++ src/EzPlatformCoreBundle/bundle/EzPlatformCoreBundle.php
@@ -19,7 +19,7 @@ final class EzPlatformCoreBundle extends Bundle
     /**
      * Ibexa DXP Version.
      */
-    public const VERSION = '3.3.36';
+    public const VERSION = '3.3.37';
 
     public function getContainerExtension(): ExtensionInterface
     {
